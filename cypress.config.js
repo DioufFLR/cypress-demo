2a936479bf30574abd3584ff709be9a54adbcc6c
Diouf <geoffrey@vizualee.com>
2025-11-03 18:29:09 +0100

    Enable video recording and configure retry logic in Cypress settings

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    video: true,
     e2e: {
         baseUrl: "https://the-internet.herokuapp.com",
         setupNodeEvents(on, config) {},
+        retries: { runMode: 1, openMode: 0}
     },
     screenshotsFolder: "cypress/artifacts/screenshots",
     videosFolder: "cypress/artifacts/videos",
